d6895f5020d2e384933755fde3c8ba764757d68a
Amandine Cr <2412770-AmandineCr@users.noreply.gitlab.com>
2021-03-25 03:40:23 +0100

    Carrier choices + order summary + save in DB + display in EasyAdmin

--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Adress;
+use App\Entity\Carrier;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,12 +18,22 @@ class OrderType extends AbstractType
 
         $builder
             ->add('adresses', EntityType::class, [
-                'label' => 'Choisissez votre adresse de livraison',
+                'label' => false,
                 'required' => true,
                 'class' => Adress::class,
                 'choices' => $user->getAdresses(),
                 'multiple' => false,
                 'expanded' => true
+            ])
+            ->add('carriers', EntityType::class, [
+                'label' => 'Choisissez votre transporteur',
+                'required' => true,
+                'class' => Carrier::class,
+                'multiple' => false,
+                'expanded' => true
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider ma commande',
             ])
         ;
     }
